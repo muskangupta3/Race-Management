d09fac40fefbecf56ecc2b644105a4c2f62b14c5
muskangupta3 <45684893+muskangupta3@users.noreply.github.com>
2018-12-08 23:20:56 +0530

    Update con.js

--- con.js
+++ con.js
@@ -1,15 +1,15 @@
-Convert into a javascript file .
 
-// var mysql = require('mysql');
 
-// var con = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "password",
-//   database : "database"
-// });
+var mysql = require('mysql');
 
-// con.connect(function(err) {
-//   if (err) throw err;
-//   console.log("Connected!");
-// });
+ var con = mysql.createConnection({
+   host: "localhost",
+   user: "root",
+   password: "password",
+   database : "database"
+ });
+
+ con.connect(function(err) {
+   if (err) throw err;
+   console.log("Connected!");
+ });
